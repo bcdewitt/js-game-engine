a8ae543ea1778b03f3965e8ba2ea03c4380a38da
BDawgonnit <dewitt_bc@hotmail.com>
2016-10-16 20:57:40 -0400

    Optimized animation; Added ability to draw a portion of a map layer (https://app.asana.com/0/152999524925421/192148355310675, https://app.asana.com/0/152999524925421/196618050937582)

--- js/TiledMap.js
+++ js/TiledMap.js
@@ -6,13 +6,14 @@ define('TiledMap', function(module) {
 
 	class TiledMap {
 
+		// Load methods
 		constructor(url, onLoadCallback) {
 			this.json = {};
 			this.tileWidth = 0;
 			this.tileHeight = 0;
 			this.tiles = [];
-			this.layers = {};
-			this.layerCanvases = {};
+			this.layers = utilities.createIterableObject();
+			this.layerCanvases = utilities.createIterableObject();
 			this.hasBGM = false;
 			this.bgm;
 
@@ -23,127 +24,119 @@ define('TiledMap', function(module) {
 				this.tileWidth = this.json.tilewidth;
 				this.tileHeight = this.json.tileheight;
 
-				// Load the tileset image files (get url from the map data)
-				let tilesets = this.json.tilesets;
-				for(let tileset of tilesets) {
-					assetManager.queueDownload(tileset.image);
-				}
+				this.queueMapFiles(assetManager);
 
-				if(this.json.properties.bgm) {
-					this.hasBGM = true;
-					assetManager.queueDownload(this.json.properties.bgm);
-				}
-
-				// TODO: Load the image files for spawnable entities (from the map data)
-
+				// Download queued files, finish initializing
 				assetManager.downloadAll(() => {
 
 					if(this.hasBGM) this.bgm = assetManager.getAsset(this.json.properties.bgm);
 
 					// Populate this.tiles array
-					let tilesets = this.json.tilesets;
-					for(let tileset of tilesets) {
-						let img = assetManager.getAsset(tileset.image);
-
-						let yInc = tileset.tileheight + tileset.spacing;
-						for(let y = tileset.margin; this.tiles.length < tileset.tilecount; y += yInc) {
-
-							let xInc = tileset.tilewidth + tileset.spacing;
-							for(let x = tileset.margin, l = (tileset.columns * tileset.tilewidth); x < l; x += xInc) {
-								let obj = {
-									img: img,
-									x: x,
-									y: y,
-									width: tileset.tilewidth,
-									height: tileset.tileheight
-								};
-								let extraData = tileset.tiles && tileset.tiles[this.tiles.length];
-
-								if(extraData && extraData.animation) {
-									let rangeStart = 0;
-									let rangeEnd = 0;
-									obj.animation = [];
-
-									for(let step of extraData.animation) {
-										rangeStart = rangeEnd;
-										rangeEnd = rangeStart + step.duration;
-										obj.animation.push({
-											rangeStart: rangeStart,
-											rangeEnd: rangeEnd,
-											tileid: step.tileid
-										});
-									}
-								}
-
-								this.tiles.push(obj);
-							}
-						}
-					}
+					this.populateTiles(this.json.tilesets);
 
 					// Split up each layer's data into x and y coordinate multidimensional array
-					let layers = this.json.layers;
-					for(let layer of layers) {
-						if(layer.data) {
-							let layerData = utilities.createArray(layer.width, layer.height);
-							let idx = 0;
+					this.populateLayers(this.json.layers);
 
-							for(let y = 0, l = layer.height; y < l; y++) {
-								for(let x = 0, l2 = layer.width; x < l2; x++) {
-									layerData[x][y] = layer.data[idx++];
-								}
-							}
-
-							this.layers[layer.name] = {
-								width: layer.width,
-								height: layer.height,
-								data: layerData
-							};
-						}
-					}
-
-					// TODO: call renderToCanvas() for each layer (avoid drawing animated tiles, that comes later)
-					// TODO: add rendered canvases to layerCanvases
+					// Draw the non-animated parts of each map layer on stored canvases (speeds up rendering at runtime)
+					this.populateLayerCanvases();
 
 					onLoadCallback();
 				});
 			});
 		}
 
-		renderToCanvas(canvas, layerName, timestamp, x1, y1, x2, y2) {
-			this.startTime = this.startTime || timestamp;
-			let time = timestamp - this.startTime;
+		queueMapFiles() {
+			// Queue the tileset image files (get url from the map data)
+			let tilesets = this.json.tilesets;
+			for(let tileset of tilesets) {
+				assetManager.queueDownload(tileset.image);
+			}
 
-			canvas = canvas || document.createElement('canvas');
+			// Queue the map's music file
+			if(this.json.properties.bgm) {
+				this.hasBGM = true;
+				assetManager.queueDownload(this.json.properties.bgm);
+			}
+		}
 
-			let context = canvas.getContext('2d');
-			let layer = this.layers[layerName];
+		populateTiles(tilesets) {
+			for(let tileset of tilesets) {
+				let img = assetManager.getAsset(tileset.image);
 
-			if(layer && layer.data) {
-				for(let y = 0, l = layer.height; y < l; y++) {
-					for(let x = 0, l2 = layer.width; x < l2; x++) {
-						let tile = this.tiles[layer.data[x][y] - 1];
-						let posX = x * this.tileWidth;
-						let posY = y * this.tileHeight;
+				let yInc = tileset.tileheight + tileset.spacing;
+				for(let y = tileset.margin; this.tiles.length < tileset.tilecount; y += yInc) {
 
-						if(tile) {
-							if(tile.animation) {
-								let wrappedTime = time % tile.animation[tile.animation.length - 1].rangeEnd;
-								for(let step of tile.animation) {
-									if(wrappedTime > step.rangeStart && wrappedTime < step.rangeEnd)
-										tile = this.tiles[step.tileid];
-								}
-								context.drawImage(
-									tile.img,
-									tile.x,
-									tile.y,
-									tile.width,
-									tile.height,
-									posX,
-									posY,
-									this.tileWidth,
-									this.tileHeight
-								);
-							} else {
+					let xInc = tileset.tilewidth + tileset.spacing;
+					for(let x = tileset.margin, l = (tileset.columns * tileset.tilewidth); x < l; x += xInc) {
+						let obj = {
+							img: img,
+							x: x,
+							y: y,
+							width: tileset.tilewidth,
+							height: tileset.tileheight
+						};
+						let extraData = tileset.tiles && tileset.tiles[this.tiles.length];
+
+						if(extraData && extraData.animation) {
+							let rangeStart = 0;
+							let rangeEnd = 0;
+							obj.animation = [];
+
+							for(let step of extraData.animation) {
+								rangeStart = rangeEnd;
+								rangeEnd = rangeStart + step.duration;
+								obj.animation.push({
+									rangeStart: rangeStart,
+									rangeEnd: rangeEnd,
+									tileid: step.tileid
+								});
+							}
+						}
+
+						this.tiles.push(obj);
+					}
+				}
+			}
+		}
+
+		populateLayers(layers) {
+			for(let layer of layers) {
+				if(layer.data) {
+					let layerData = utilities.createArray(layer.width, layer.height);
+					let idx = 0;
+
+					for(let y = 0, l = layer.height; y < l; y++) {
+						for(let x = 0, l2 = layer.width; x < l2; x++) {
+							layerData[x][y] = layer.data[idx++];
+						}
+					}
+
+					this.layers[layer.name] = {
+						width: layer.width,
+						height: layer.height,
+						data: layerData
+					};
+				}
+			}
+		}
+
+		populateLayerCanvases() {
+
+			for (let layerName in this.layers) {
+				let layer = this.layers[layerName];
+				let canvas = document.createElement('canvas');
+				canvas.width = layer.width * this.tileWidth;
+				canvas.height = layer.height * this.tileHeight;
+				let context = canvas.getContext('2d');
+
+				if(layer && layer.data) {
+					for(let y = 0, l = layer.height; y < l; y++) {
+						for(let x = 0, l2 = layer.width; x < l2; x++) {
+							let tile = this.tiles[layer.data[x][y] - 1];
+							let posX = x * this.tileWidth;
+							let posY = y * this.tileHeight;
+
+							if(tile && tile.animation === undefined) {
 								context.drawImage(
 									tile.img,
 									tile.x,
@@ -156,13 +149,76 @@ define('TiledMap', function(module) {
 									this.tileHeight
 								);
 							}
+
+						}
+					}
+				}
+
+				this.layerCanvases[layerName] = canvas;
+			}
+
+		}
+
+
+		// Render methods
+		renderAnimatedTiles(context, layerName, time, tileX1, tileY1, tileX2, tileY2) {
+			let layer = this.layers[layerName];
+
+			if(layer && layer.data) {
+				for(let y = tileY1, l = Math.min(layer.height, tileY2); y < l; y++) {
+					for(let x = tileX1, l2 =  Math.min(layer.width, tileX2); x < l2; x++) {
+						let tile = this.tiles[layer.data[x][y] - 1];
+						let posX = x * this.tileWidth;
+						let posY = y * this.tileHeight;
+
+						if(tile && tile.animation) {
+							let wrappedTime = time % tile.animation[tile.animation.length - 1].rangeEnd;
+							for(let step of tile.animation) {
+								if(wrappedTime > step.rangeStart && wrappedTime < step.rangeEnd)
+									tile = this.tiles[step.tileid];
+							}
+
+							context.drawImage(
+								tile.img,
+								tile.x,
+								tile.y,
+								tile.width,
+								tile.height,
+								posX,
+								posY,
+								this.tileWidth,
+								this.tileHeight
+							);
+
 						}
 					}
 				}
 			}
 
-			return canvas;
 		}
+
+		render(context, layerName, timestamp, x1, y1, width, height) {
+			// Note: May need to use context.getImageData() and .putImageData() for transparency support instead of .drawImage()
+			// ...I tried these but they created memory leaks when debugging with Chrome
+
+			this.startTime = this.startTime || timestamp;
+
+			// Draw static parts of layer
+			context.drawImage(this.layerCanvases[layerName], x1, y1, width, height, 0, 0, width, height);
+
+			// Draw animated parts of layer
+			this.renderAnimatedTiles(
+				context,
+				layerName,
+				(timestamp - this.startTime),    // get time since first render (for animation)
+				(x1 / this.tileWidth),           // calc x1 in tile units
+				(y1 / this.tileHeight),          // calc y1 in tile units
+				(x1 + width) / this.tileWidth,   // calc x2 in tile units
+				(y1 + height) / this.tileHeight  // calc y2 in tile units
+			);
+
+		}
+
 	}
 
 	module.exports = TiledMap;
--- js/gameEngine.js
+++ js/gameEngine.js
@@ -1,21 +1,30 @@
 define('GameEngine', function(module) {
 	'use strict';
 
+	const utilities = require('utilities');
 	const TiledMap = require('TiledMap');
 
 	class GameEngine {
 		constructor(jsonPath) {
 			this.loaded = false;
+			this.initiated = false;
 			this.runAfterLoad = false;
 			this.canvas = document.getElementById('game');
+			this.systems = utilities.createIterableObject();
+			this.entities = [];
 
 			// Set up map
 			this.map = new TiledMap(jsonPath, () => {
 				this.loaded = true;
 
-				// *call this.run() if an attempt was made before loading was complete
+				// *call this.run() if an attempt was made to call this.run() before loading was completed
 				if(this.runAfterLoad) { this.run(); }
 			});
+		}
+
+		init() {
+			// TODO: Init and add each System
+			this.initiated = true;
 		}
 
 		run() {
@@ -26,14 +35,21 @@ define('GameEngine', function(module) {
 				return;
 			}
 
-			let main = (timestamp) => {
-				window.requestAnimationFrame(main); // This can also return a callback id
+			if(!this.initiated) this.init();
 
-				// TODO: Replace this with actual ECS setup
-				this.map.renderToCanvas(this.canvas, 'Background', timestamp);
-				this.map.renderToCanvas(this.canvas, 'Platforms', timestamp);
+			let context = this.canvas.getContext('2d');
+
+			// Define the main loop logic
+			let main = (timestamp) => {
+
+				// TODO: Replace this with actual ECS setup (loop over Systems, RenderSystem will handle context and map.render calls)
+				this.map.render(context, 'Background', timestamp, 0, 0, this.canvas.width, this.canvas.height);
+				this.map.render(context, 'Platforms', timestamp, 0, 0, this.canvas.width, this.canvas.height);
+
+				window.requestAnimationFrame(main);
 			};
 
+			// Run main loop (wrapper handles loop logic, main)
 			main(performance.now());
 		}
 	}
